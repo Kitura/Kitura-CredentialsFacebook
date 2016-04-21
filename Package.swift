7ebcc330ef721309251921b1b30d495cdbb639cb
Shmuel Kallner <kallner@il.ibm.com>
2016-04-21 18:12:22 +0300

    IBM-Swift/Kitura#403 Updated to latest version in master

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ import PackageDescription
 let package = Package(
     name: "Kitura-CredentialsFacebook",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 0, minor: 10),
     ]
 )
